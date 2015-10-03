4e1f462189c73e00dffefe9fdf865ec4de58d1f8
rkunev <rkunev@gmail.com>
2015-10-03 12:26:29 +0300

    Add video embed for youtube

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,83 +1,43 @@
 var Popup = function() {
 
-    // singletonPrivateVar
-    var singletonPrivateVar = false;
+    var handleVideoEmbed = function() {
+        function VideoModel() {
+            this.normalURL = ko.observable();
 
-    // initializes main settings
-    var handleInit = function() {
-
-        if ($('body').css('direction') === 'rtl') {
-            isRTL = true;
+            this.embeddedURL = ko.pureComputed({
+                read: function () {
+                    var urlParams = getEmbeddedURL( this.normalURL() );
+                    return urlParams;
+                },
+                owner: this
+            });
         }
 
-        isIE8 = !!navigator.userAgent.match(/MSIE 8.0/);
-        isIE9 = !!navigator.userAgent.match(/MSIE 9.0/);
-        isIE10 = !!navigator.userAgent.match(/MSIE 10.0/);
-
-        if (isIE10) {
-            $('html').addClass('ie10'); // detect IE10 version
-        }
-
-        if (isIE10 || isIE9 || isIE8) {
-            $('html').addClass('ie'); // detect IE10 version
-        }
+        ko.applyBindings( new VideoModel() );
     };
 
+    var getEmbeddedURL = function( normalURL ) {
+        var videoId = getParameterFromStringByName( normalURL, 'v' ),
+            normalURL = 'https://www.youtube.com/embed/';
 
-    // Handles Bootstrap Modals.
-    var handleModals = function() {
-        // fix stackable modal issue: when 2 or more modals opened, closing one of modal will remove .modal-open class.
-        $('body').on('hide.bs.modal', function() {
-            if ($('.modal:visible').size() > 1 && $('html').hasClass('modal-open') === false) {
-                $('html').addClass('modal-open');
-            } else if ($('.modal:visible').size() <= 1) {
-                $('html').removeClass('modal-open');
-            }
-        });
+        return videoId ? normalURL + videoId : videoId;
+    };
 
-        // fix page scrollbars issue
-        $('body').on('show.bs.modal', '.modal', function() {
-            if ($(this).hasClass("modal-scroll")) {
-                $('body').addClass("modal-open-noscroll");
-            }
-        });
-
-        // fix page scrollbars issue
-        $('body').on('hide.bs.modal', '.modal', function() {
-            $('body').removeClass("modal-open-noscroll");
-        });
-
-        // remove ajax content and remove cache on modal closed
-        $('body').on('hidden.bs.modal', '.modal:not(.modal-cached)', function () {
-            $(this).removeData('bs.modal');
-        });
+    var getParameterFromStringByName = function( str, name ) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(str);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     };
 
     return {
 
-        /** main function to initiate the theme */
+        /** kickstart the page */
         init: function() {
-            // initialize core variables
-            handleInit();
-
-            // UI Component handlers
-            handleModals(); // handle modals
+            // initialize video bind and embed
+            handleVideoEmbed();
         },
 
         /** public functions and helper methods */
-
-        // Get a parameter by name from URL
-        getURLParameter: function(paramName) {
-            var searchString = window.location.search.substring(1),
-                i, val, params = searchString.split("&");
-
-            for (i = 0; i < params.length; i++) {
-                val = params[i].split("=");
-                if (val[0] == paramName) {
-                    return unescape(val[1]);
-                }
-            }
-            return null;
-        }
     };
 }();
